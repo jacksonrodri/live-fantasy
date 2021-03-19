c598c037c414ae4dc831277b2f683989e31cff96
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-03-19 19:23:20 +0500

    update  balance

--- src/components/AccountInfoComponent/index.js
+++ src/components/AccountInfoComponent/index.js
@@ -1,60 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-import classes from './index.module.scss';
+import classes from "./index.module.scss";
 
 function AccountInfo(props) {
-    const { isMobile = false } = props || {};
-
-    const renderItem = (title, value, buttonTitle) => {
-        const onButtonClick = () => {
-            console.log('button clicked')
-        }
-        return (
-            <div className={classes.list_item}>
-                <span>{title}</span>
-                <span>
-                    <span>{value}</span>
-                    <button onClick={onButtonClick}>{ buttonTitle ? buttonTitle : 'Edit' }</button>
-                </span>
-            </div>
-        )
-    }
+  const { isMobile = false } = props || {};
+  const { user = {} } = props || {};
+  useEffect(() => {
+    console.log(props);
+  }, []);
+  const renderItem = (title, value, buttonTitle) => {
+    const onButtonClick = () => {};
     return (
-        <div className={classes.list_container}>
-            {
-                renderItem('Full Name', 'My Full Name')
-            }
+      <div className={classes.list_item}>
+        <span>{title}</span>
+        <span>
+          <span>{value}</span>
+          <button onClick={onButtonClick}>
+            {buttonTitle ? buttonTitle : "Edit"}
+          </button>
+        </span>
+      </div>
+    );
+  };
+  return (
+    <div className={classes.list_container}>
+      {renderItem("Full Name", user.firstName)}
 
-            {
-                renderItem('Display Name', 'My Full Name')
-            }
+      {renderItem("Display Name", "My Full Name")}
 
-            {
-                renderItem('Email', 'abc@email.com')
-            }
-            {
-                renderItem('Date of Birth', 'January 1, 1990')
-            }
+      {renderItem("Email", "abc@email.com")}
+      {renderItem("Date of Birth", "January 1, 1990")}
 
-            {
-                renderItem('Country', 'Pakistan')
-            }
+      {renderItem("Country", "Pakistan")}
 
-            {
-                renderItem(isMobile ? 'Province' : 'Province/State/Territory', 'Islamabad')
-            }
+      {renderItem(
+        isMobile ? "Province" : "Province/State/Territory",
+        "Islamabad"
+      )}
 
-            {
-                renderItem('Change Password', '', 'Change')
-            }
-        </div>
-    )
+      {renderItem("Change Password", "", "Change")}
+    </div>
+  );
 }
 
 AccountInfo.propTypes = {
-    isMobile: PropTypes.bool,
-}
+  isMobile: PropTypes.bool,
+};
 
-export default AccountInfo
-
+export default AccountInfo;

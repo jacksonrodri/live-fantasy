583ebc4bf0c0a34de769a6f42d3c819ce5dfeabc
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-04-02 08:44:47 +0500

    develop-the-contest-rules-popup

--- src/components/Header3/index.js
+++ src/components/Header3/index.js
@@ -4,26 +4,28 @@ import PropTypes from 'prop-types';
 import classes from './index.module.scss';
 import DocIcon from '../../icons/Doc';
 import Trophy from '../../icons/Trophy';
+import ContestRulesPopUp from '../ContestRulesPopUp';
 
 function Header3(props) {
-    const { onClickContest = () => {}, onClickPrize = () => {}, titleMain1 = '', titleMain2 = '', subHeader1 = '', subHeader2 = '', contestBtnTitle = '', prizeBtnTitle = '', bgImageUri } = props || {};
-
+    const { onClickContest = () => { }, onClickPrize = () => { }, titleMain1 = '', titleMain2 = '', subHeader1 = '', subHeader2 = '', contestBtnTitle = '', prizeBtnTitle = '', bgImageUri } = props || {};
     return (
-        <div className={classes.header_container} style={{backgroundImage: 'url(' + bgImageUri + ')'}}>
-            {
-                titleMain1 &&
-                <div className={classes.header_title}>
-                    <span className={classes.line} /><h2>{titleMain1} <span>{titleMain2}</span></h2><span className={classes.line} />
-                </div>
-            }
-            { subHeader1 && <p>{subHeader1}</p> }
-            { subHeader2 && <p className={classes.p2}>{subHeader2}</p> }
+        <>
+            <div className={classes.header_container} style={{ backgroundImage: 'url(' + bgImageUri + ')' }}>
+                {
+                    titleMain1 &&
+                    <div className={classes.header_title}>
+                        <span className={classes.line} /><h2>{titleMain1} <span>{titleMain2}</span></h2><span className={classes.line} />
+                    </div>
+                }
+                {subHeader1 && <p>{subHeader1}</p>}
+                {subHeader2 && <p className={classes.p2}>{subHeader2}</p>}
 
-            <div className={classes.header_buttons}>
-                { contestBtnTitle && <button onClick={onClickContest}><DocIcon /> Contest Rules</button> }
-                { prizeBtnTitle && <button onClick={onClickPrize}><Trophy /> Prize Grid</button> }
+                <div className={classes.header_buttons}>
+                    <ContestRulesPopUp component={({ showPopUp }) => <button onClick={showPopUp}><DocIcon /> Contest Rules</button>} />
+                    {prizeBtnTitle && <button onClick={onClickPrize}><Trophy /> Prize Grid</button>}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 

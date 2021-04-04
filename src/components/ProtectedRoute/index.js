4158b859f60fe392564bfe388d3d17677dc7e2dc
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-04-04 20:36:30 +0500

    re-added-the-protected-route

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -3,24 +3,24 @@ import { Redirect, Route } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
 import { isEmpty } from 'lodash';
-import {  getLocalStorage } from '../../utility/shared';
+import { getLocalStorage } from '../../utility/shared';
 import { CONSTANTS } from '../../utility/constants';
 
 function ProtectedRoute({ component: Component, ...rest }) {
     const { use: { token = '' } = {} } = useSelector((state) => state?.auth);
     let isAuthenticated = getLocalStorage(CONSTANTS.LOCAL_STORAGE_KEYS.USER)
-    
+
     return <Route {...rest} render={(props) => {
         return (
-            <Component {...props} />
-            // isAuthenticated ?
-            //     :
-            //     <Redirect to={{
-            //         pathname: '/login',
-            //         state: {from: props?.location}
-            //     }} />
-            )
-        }} />
+            isAuthenticated ?
+                <Component {...props} />
+                :
+                <Redirect to={{
+                    pathname: '/login',
+                    state: { from: props?.location }
+                }} />
+        )
+    }} />
 }
 
 export default React.memo(ProtectedRoute)

9ebca2f0e1852b9421416aeece0b2822a83532e7
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-04-04 09:52:31 +0500

    optimized-the-code

--- src/ui/PopUp/index.js
+++ src/ui/PopUp/index.js
@@ -1,26 +1,6 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import styles from './styles.module.scss';
+import React from 'react';
 import './styles.scss';
 import ReactDOM from 'react-dom';
 
-const PopUpWrapper = ({ children, className, onClose }) => {
-    const popupRef = useRef(null);
-    useEffect(() => {
-        if (!popupRef) return;
-        const { offsetHeight, classList } = popupRef.current;
-        offsetHeight < window.innerHeight && classList.add('popup-with-fixed');
-    }, [])
-    return (
-        <>
-            <div className={styles.root} ref={popupRef}>
-                <div className='blur' onClick={onClose}></div>
-                <div className={`${styles.popup} ${className} popup-wrapper`}>
-                    {children}
-                </div>
-            </div>
-        </>
-    )
-}
-
-const PopUp = props => ReactDOM.createPortal(<PopUpWrapper {...props} />, document.getElementById('popup'))
+const PopUp = props => ReactDOM.createPortal(props.children, document.getElementById('popup'))
 export default PopUp;

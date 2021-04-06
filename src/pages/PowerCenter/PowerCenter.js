fefa4bd34331e8a3d7c346f67ea609d74f95ad23
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-04-06 16:43:10 +0500

    power center currency new ui completed and account limits tab added to my account

--- src/pages/PowerCenter/PowerCenter.js
+++ src/pages/PowerCenter/PowerCenter.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink, Route } from 'react-router-dom';
 import Header from '../../components/Header/Header';
@@ -11,12 +11,31 @@ import PowerPlayIcon from '../../assets/powerplay-icon.png';
 import PowerCenterBannerTitleIcon from '../../assets/power-center-banner-title-icon.png';
 import PowerBalanceGrey from '../../assets/power-balance-grey.png';
 import CashBalanceGrey from '../../assets/cash-balance-grey.png';
+import BitcoinGrey from '../../assets/bitcoin-grey.png';
+import EthereumGrey from '../../assets/ethereum-grey.png';
 import { getLocalStorage, redirectTo } from '../../utility/shared';
 import { CONSTANTS } from '../../utility/constants';
+
+const CURRENCY_DATA = [
+    {
+        label: 'Cash Balance',
+        value: 'cash'
+    },
+    {
+        label: 'Bitcoin Balance',
+        value: 'bitcoin'
+    },
+    {
+        label: 'Ethereum Balance',
+        value: 'ethereum'
+    },
+];
 
 const PowerCenter = props => {
     const { url } = props.match;
     const { auth: { user: { token = '' } }, user: {userBalance = {}} = {} } = useSelector((state) => state);
+    const [currencyMenu, setCurrencyMenu] = useState(false);
+    const [displayCurrency, setDisplayCurrency] = useState('default');
 
     return (
         <Fragment>
@@ -40,37 +59,107 @@ const PowerCenter = props => {
                         token || getLocalStorage(CONSTANTS.LOCAL_STORAGE_KEYS.USER)
                         ?
                         <div className='__power_center_banner_footer'>
-                        <div className='__power_center_banner_footer_deposit' onClick={() => redirectTo(props, { path: "/my-account" })}>
-                            Deposit
+                            <div className='__power_center_banner_footer_deposit' onClick={() => redirectTo(props, { path: "/my-account" })}>
+                                Deposit
+                            </div>
+                            <div className='__power_center_banner_footer_cash_and_balance_outer border_right'>
+                                <div className='__power_center_banner_footer_cash_and_balance_icon'>
+                                    <img src={PowerBalanceGrey} />
+                                </div>
+                                <div className='__power_center_banner_footer_cash_and_balance_inner'>
+                                    <div className='__power_center_banner_footer_power_and_cash_balance'>
+                                        {userBalance.tokenBalance || getLocalStorage(CONSTANTS.LOCAL_STORAGE_KEYS.TOKEN_BALANCE)}
+                                    </div>
+                                    <div className='__power_center_banner_footer_power_and_cash_balance_title'>
+                                        Power Balance
+                                    </div>
+                                </div>
+                            </div>
+                            {
+                                (displayCurrency == 'default' || displayCurrency == 'cash')
+                                &&
+                                <div className={`${'__power_center_banner_footer_cash_and_balance_outer'} ${displayCurrency == 'default' && 'border_right'}`}>
+                                    <div className='__power_center_banner_footer_cash_and_balance_icon'>
+                                        <img src={CashBalanceGrey} />
+                                    </div>
+                                    <div className='__power_center_banner_footer_cash_and_balance_inner'>
+                                        <div className='__power_center_banner_footer_power_and_cash_balance'>
+                                            ${userBalance.cashBalance || getLocalStorage(CONSTANTS.LOCAL_STORAGE_KEYS.CASH_BALANCE)}
+                                        </div>
+                                        <div className='__power_center_banner_footer_power_and_cash_balance_title'>
+                                            Cash Balance
+                                        </div>
+                                    </div>
+                                </div>
+                            }
+                            {
+                                (displayCurrency == 'default' || displayCurrency == 'bitcoin')
+                                &&
+                                <div className={`${'__power_center_banner_footer_cash_and_balance_outer'} ${displayCurrency == 'default' && 'border_right'}`}>
+                                    <div className='__power_center_banner_footer_cash_and_balance_icon'>
+                                        <img src={BitcoinGrey} />
+                                    </div>
+                                    <div className='__power_center_banner_footer_cash_and_balance_inner'>
+                                        <div className='__power_center_banner_footer_power_and_cash_balance'>
+                                            .00241
+                                        </div>
+                                        <div className='__power_center_banner_footer_power_and_cash_balance_title'>
+                                            Bitcoin 
+                                        </div>
+                                    </div>
+                                </div>
+                            }
+                            {
+                                (displayCurrency == 'default' || displayCurrency == 'ethereum')
+                                &&
+                                    <div className='__power_center_banner_footer_cash_and_balance_outer'>
+                                    <div className='__power_center_banner_footer_cash_and_balance_icon'>
+                                        <img src={EthereumGrey} />
+                                    </div>
+                                    <div className='__power_center_banner_footer_cash_and_balance_inner'>
+                                        <div className='__power_center_banner_footer_power_and_cash_balance'>
+                                            .007
+                                        </div>
+                                        <div className='__power_center_banner_footer_power_and_cash_balance_title'>
+                                            Ethereum
+                                        </div>
+                                    </div>
+                                </div>
+                            }
+                            {
+                                currencyMenu
+                                &&
+                                <div className='__currency_menu'>
+                                    <div>
+                                        Display:
+                                    </div>
+                                    {
+                                        CURRENCY_DATA.map((item, index) => {
+                                            return (
+                                                <div 
+                                                    className={
+                                                        `${'__currency_menu_item'} 
+                                                        ${displayCurrency == item.value && '__currency_menu_selected'}`
+                                                    }
+                                                    onClick={() => {
+                                                        setDisplayCurrency(item.value);
+                                                        setCurrencyMenu(false);
+                                                    }}>
+                                                    {item.label}
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                </div>
+                            }
+                            <div className='__three_dots_div'>
+                                <button className='__three_dots' onClick={() => setCurrencyMenu(!currencyMenu)}>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </button>
+                            </div>
                         </div>
-                        <div className='__power_center_banner_footer_cash_and_balance_outer border_right'>
-                            <div className='__power_center_banner_footer_cash_and_balance_icon'>
-                                <img src={PowerBalanceGrey} />
-                            </div>
-                            <div className='__power_center_banner_footer_cash_and_balance_inner'>
-                                <div className='__power_center_banner_footer_power_and_cash_balance'>
-                                    {userBalance.tokenBalance || getLocalStorage(CONSTANTS.LOCAL_STORAGE_KEYS.TOKEN_BALANCE)}
-                                </div>
-                                <div className='__power_center_banner_footer_power_and_cash_balance_title'>
-                                    Power Balance
-                                </div>
-                            </div>
-                        </div>
-                        <div className='__power_center_banner_footer_cash_and_balance_outer'>
-                            <div className='__power_center_banner_footer_cash_and_balance_icon'>
-                                <img src={CashBalanceGrey} />
-                            </div>
-                            <div className='__power_center_banner_footer_cash_and_balance_inner'>
-                                <div className='__power_center_banner_footer_power_and_cash_balance'>
-                                    ${userBalance.cashBalance || getLocalStorage(CONSTANTS.LOCAL_STORAGE_KEYS.CASH_BALANCE)}
-                                </div>
-                                <div className='__power_center_banner_footer_power_and_cash_balance_title'>
-                                    Cash Balance
-                                </div>
-                            </div>
-                        </div>
-                        <div style={{width: 80}} />
-                    </div>
                     :
                     <div style={{ height: 50}}></div>
                     }

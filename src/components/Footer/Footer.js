aa960c30490e2e3672553c92e01b1c5f418aa4c4
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-04-11 21:04:50 +0500

    redesigned-the-footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import logo from '../../assets/new-logo.png';
+import logo from '../../assets/new-logo-2.png';
 import { Link } from 'react-router-dom';
 import styles from './index.module.scss';
 
@@ -6,22 +6,24 @@ import styles from './index.module.scss';
 const Footer = props => {
     return (
         <footer className={`${styles.root} ${props.className} ${props.isBlack ? styles.style_2 : ''}`}>
-            <Link to='/'><img src={logo} alt='' className={styles.logoWrapper} /></Link>
-            <div className={styles.linksWrapper}>
-                <span>Essentials</span>
-                <span>Legal Privacy</span>
-                <span>Company</span>
-                <Link to='/my-account'>My Account</Link>
-                <Link to='/terms'>Terms of use</Link>
-                <Link to='/faqs'>FAQs</Link>
-                <Link to='/sponer-contest'>Sponsor a contest</Link>
-                <Link to='/privacy'>Privacy</Link>
-                <Link to='/trust-and-safety'>Trust & Safety </Link>
-                <span></span>
-                <Link to='/account-security'>Account Security</Link>
-                <Link to='/about-us'>About Us</Link>
+            <div className={styles.container}>
+                <Link to='/'><img src={logo} alt='' className={styles.logo} /></Link>
+                <div className={styles.linksWrapper}>
+                    <span>Essentials</span>
+                    <span>Legal Privacy</span>
+                    <span>Company</span>
+                    <Link to='/my-account'>My Account</Link>
+                    <Link to='/terms'>Terms of use</Link>
+                    <Link to='/faqs'>FAQs</Link>
+                    <Link to='/sponer-contest'>Sponsor a contest</Link>
+                    <Link to='/privacy'>Privacy</Link>
+                    <Link to='/trust-and-safety'>Trust & Safety </Link>
+                    <Link to='/responsible-gaming'>Responsible Gaming</Link>
+                    <Link to='/account-security'>Account Security</Link>
+                    <Link to='/about-us'>About Us</Link>
+                </div>
+                <p className={styles.copyWriteSection}>Copyright © 2021 PowerPlay Systems Inc. - All Rights Reserved</p>
             </div>
-            <p className={styles.copyWriteSection}>Copyright © 2021 PowerPlay Systems Inc. - All Rights Reserved <br />140 Yonge St., S.200, Toronto, ON, M5C 1X6 | info@powerplaysystems.com | 1-888-808-7697</p>
         </footer>
     );
 }

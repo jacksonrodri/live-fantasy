f2316c6eee0bdf2809ce4b13d82cf8fbcf6d8c6d
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-04-23 11:50:13 +0500

    MLB player selection

--- src/config/http.js
+++ src/config/http.js
@@ -5,7 +5,7 @@ import { CONSTANTS } from "../utility/constants";
 const apiUrl = "http://api.powerplaysystems.com/api/v1";
 
 const http = axios.create({
-  baseURL: apiUrl,
+  baseURL: `${process.env.REACT_APP_API_URL}/api/v1` || apiUrl,
   timeout: 60000, //60 seconds timeout
   timeoutErrorMessage: "Request time out, please try again later",
   maxRedirects: 3,

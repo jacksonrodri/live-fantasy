922afe4dff516cd0ce7f2b0d267efdf3a11bd877
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-04-27 10:42:51 +0500

    MLB score detail page.

--- src/pages/MLBLivePowerdFsScroeDetail/MLBLivePowerdFsScroeDetail.js
+++ src/pages/MLBLivePowerdFsScroeDetail/MLBLivePowerdFsScroeDetail.js
@@ -5,7 +5,7 @@ import classes from "./index.module.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Header3 from "../../components/Header3";
-import HeaderBgUri from "../../assets/nhl-live.jpg";
+import HeaderBgUri from "../../assets/baseball.jpg";
 import NHLLiveSportsHeader from "../../components/NHLLiveSportsHeader";
 import Card from "../../components/PowerpickCard";
 import SidebarBtnIcon from "../../assets/nhl-sidebar-icon.png";
@@ -14,6 +14,9 @@ import SportsContestRules from "../../components/SportsContestRules";
 import MLBFooterImage from "../../assets/NHL.png";
 import NHLGear from "../../assets/nhl-gear.png";
 import LiveStandings from "../../components/LiveStandings";
+import XP2Icon from "../../icons/XP2";
+import XP3Icon from "../../icons/XP3";
+import XP1_5Icon from "../../icons/XP1_5";
 
 const basicRules = [
   "No purchase necessary.",
@@ -40,6 +43,25 @@ function NHLLivePowerdFsScroeDetail(props) {
     setModalState(!showModal);
   };
 
+  const RenderXP = (xp) => {
+    switch (xp) {
+      case "1.5":
+      case "1_5":
+        return <XP1_5Icon />;
+
+      case 2:
+      case "2":
+        return <XP2Icon />;
+
+      case 3:
+      case "3":
+        return <XP3Icon />;
+
+      default:
+        return "-";
+    }
+  };
+
   const Row = ({
     position,
     name,
@@ -50,6 +72,8 @@ function NHLLivePowerdFsScroeDetail(props) {
     powers,
     score,
     runningTotal,
+    rbi = {},
+    runs = {},
   }) => (
     <div
       className={`${classes.card_row} ${classes.card_row_1} ${
@@ -67,8 +91,27 @@ function NHLLivePowerdFsScroeDetail(props) {
           <p className={classes.secondary}> {pts}</p>
         </span>
       </div>
+
+      <div className={classes.card_combine_row}>
+        <span>
+          <p className={classes.primary}>{runs?.rs}</p>
+        </span>
+        <span>
+          <p className={classes.secondary}> {runs?.pts}</p>
+        </span>
+      </div>
+
+      <div className={classes.card_combine_row}>
+        <span>
+          <p className={classes.primary}>{rbi?.rbi}</p>
+        </span>
+        <span>
+          <p className={classes.secondary}> {rbi?.pts}</p>
+        </span>
+      </div>
+
       {/* <span className={`${classes.child_4} ${classes.center}`}><p className={classes.secondary}>{totalPts}</p></span> */}
-      <span className={classes.center}>{powers}</span>
+      <span className={classes.center}>{RenderXP(powers)}</span>
       <span className={classes.center}>
         <p className={score < 0 ? classes.danger : classes.success}>
           {score < 0 ? `Reversed ${score}` : score}
@@ -85,13 +128,13 @@ function NHLLivePowerdFsScroeDetail(props) {
       <Header />
       <div className={classes.wrapper}>
         <Header3
-          titleMain1="NHL 2021"
+          titleMain1="MLB 2021"
           titleMain2="PowerdFS"
           contestBtnTitle="Contest Rules"
           prizeBtnTitle="Prize Grid"
           subHeader1="Introducing Live-Play Fantasy Hockey"
-          subHeader2="Play for your chance to win $1000!"
           bgImageUri={HeaderBgUri}
+          isLive
         />
 
         <div className={classes.container}>
@@ -128,6 +171,20 @@ function NHLLivePowerdFsScroeDetail(props) {
                       <span>Pts</span>
                     </div>
                   </div>
+                  <div className={classes.card_header_1}>
+                    <p>Runs</p>
+                    <div className={classes.card_combine_row}>
+                      <span>RS</span>
+                      <span>Pts</span>
+                    </div>
+                  </div>
+                  <div className={classes.card_header_1}>
+                    <p>RBI</p>
+                    <div className={classes.card_combine_row}>
+                      <span>RBI</span>
+                      <span>Pts</span>
+                    </div>
+                  </div>
                   {/* <span className={classes.child_4}>Total Pts</span> */}
                   <span className={classes.center}>Powers</span>
                   <span className={classes.center}>My Score</span>
@@ -143,9 +200,17 @@ function NHLLivePowerdFsScroeDetail(props) {
                   plays="g"
                   pts="6"
                   totalPts="8"
-                  powers="-"
+                  powers="1.5"
                   score={16}
                   runningTotal="16"
+                  runs={{
+                    rs: 2,
+                    pts: 4,
+                  }}
+                  rbi={{
+                    rbi: 8,
+                    pts: 1,
+                  }}
                 />
 
                 <Row
@@ -155,9 +220,17 @@ function NHLLivePowerdFsScroeDetail(props) {
                   plays="g"
                   pts="6"
                   totalPts="8"
-                  powers="-"
+                  powers="2"
                   score={16}
                   runningTotal="16"
+                  runs={{
+                    rs: 2,
+                    pts: 4,
+                  }}
+                  rbi={{
+                    rbi: 8,
+                    pts: 1,
+                  }}
                 />
 
                 <Row
@@ -167,9 +240,17 @@ function NHLLivePowerdFsScroeDetail(props) {
                   plays="g"
                   pts="6"
                   totalPts="8"
-                  powers="-"
+                  powers="3"
                   score={16}
                   runningTotal="16"
+                  runs={{
+                    rs: 2,
+                    pts: 4,
+                  }}
+                  rbi={{
+                    rbi: 8,
+                    pts: 1,
+                  }}
                 />
 
                 <Row
@@ -179,9 +260,17 @@ function NHLLivePowerdFsScroeDetail(props) {
                   plays="g"
                   pts="6"
                   totalPts="8"
-                  powers="-"
+                  powers="2"
                   score={16}
                   runningTotal="16"
+                  runs={{
+                    rs: 2,
+                    pts: 4,
+                  }}
+                  rbi={{
+                    rbi: 8,
+                    pts: 1,
+                  }}
                 />
 
                 <Row
@@ -191,9 +280,17 @@ function NHLLivePowerdFsScroeDetail(props) {
                   plays="g"
                   pts="6"
                   totalPts="8"
-                  powers="-"
+                  powers="2"
                   score={-1}
                   runningTotal="16"
+                  runs={{
+                    rs: 2,
+                    pts: 4,
+                  }}
+                  rbi={{
+                    rbi: 8,
+                    pts: 1,
+                  }}
                 />
 
                 <Row
@@ -203,9 +300,17 @@ function NHLLivePowerdFsScroeDetail(props) {
                   plays="g"
                   pts="6"
                   totalPts="8"
-                  powers="-"
+                  powers="2"
                   score={16}
                   runningTotal="16"
+                  runs={{
+                    rs: 2,
+                    pts: 4,
+                  }}
+                  rbi={{
+                    rbi: 8,
+                    pts: 1,
+                  }}
                 />
               </div>
             </Card>

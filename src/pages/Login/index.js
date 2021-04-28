67a9206cf4f46b3b38c9d24c68cecad073dea294
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-04-28 14:59:56 +0500

    redesigned the login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,19 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 
-import classes from './index.module.scss'
+import styles from './styles.module.scss'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 import SignInImage from '../../assets/signin-background.png'
-import Input from '../../components/Input'
+import Input from "../../ui/Input/Input";
 import { CONSTANTS } from '../../utility/constants'
 import { authenticate } from '../../actions/authActions';
 import Alert from '../../components/Alert';
 import { isEmpty } from 'lodash';
 import { getLocalStorage, redirectTo } from '../../utility/shared';
+import formStyles from '../../scss/formstyles.module.scss';
+import HeroSection from "../../components/CreateAccountsHeroSection/HeroSection";
 
 function LoginPage(props) {
     const [user, setUser] = useState({ email: '', password: '' });
@@ -19,16 +21,16 @@ function LoginPage(props) {
     const dispatch = useDispatch();
     const { loading = false, user: authUser = {} } = useSelector((state) => state.auth);
     const { token = '', status: loggedIn = false, message = '' } = authUser || {}
-    
+
     useEffect(() => {
         if (loggedIn === true && !isEmpty(authUser)) {
-            redirectTo(props, {path: '/my-game-center'})
+            redirectTo(props, { path: '/my-game-center' })
         }
     }, [loggedIn, authUser])
-    
+
     const onLoginSubmit = async (e) => {
         e?.preventDefault();
-        
+
         if (isEmpty(user.email) || isEmpty(user.password)) {
             //error message
             return;
@@ -36,11 +38,11 @@ function LoginPage(props) {
 
         dispatch(authenticate(user));
     }
-    
+
     const redirect = () => {
         if (!isEmpty(token) || !isEmpty(getLocalStorage(CONSTANTS.LOCAL_STORAGE_KEYS.USER))) {
             const { location: { state = {} } = {} } = props || {}
-            
+
             return <Redirect to={!isEmpty(state) ? state?.from : '/'} />
         }
     }
@@ -48,63 +50,36 @@ function LoginPage(props) {
     return (
         <>
             <Header />
-            <div className={classes.wrapper}>
-                <div className={classes.back_drop} />
-                <div className={classes.header}>
-                    <img src={SignInImage} alt="" className={classes.header_image} />
-                    
-                    <div className={classes.header_content}>
-                        <p className={`${classes.header_title} mb-30`}>Get ready to Power-up!</p>
-                        <p className={classes.header_p}>
-                        Let’s start your new experience our ground-breaking live-play games where you have the Power to control your destiny!
-                        </p>
-                    </div>
-                </div>
-
-                <div className={classes.content_wrapper}>
-                    <div className={classes.content_top_skew} />
-                    <div className={`${classes.content_card} p-normal`}>
-                        {
-                            !isEmpty(authUser) && !loggedIn &&
-                            <>
-                                <Alert renderMsg={() => <p>{message}</p>} danger />
-                                <br />
-                            </>
-                        }
-                        {
-                            !isEmpty(authUser) && loggedIn &&
-                            <>
-                                <Alert renderMsg={() => <p>Login Success</p>} success />
-                                <br />
-                            </>
-                        }
-                        <form onSubmit={onLoginSubmit} className={`${classes.form} mx-50 m-sm-30`}>
-                            <Input type="text" block rounded label="E-mail" required value={user.email} onChange={(e) => {
-                                setUser({...user, email: e?.target?.value})
-                            }} bordered />
-                            <Input type="password" block rounded label="Password" required value={user.password} onChange={(e) => {
-                                setUser({...user, password: e?.target?.value})
-                            }} bordered />
-                            <button className={`${'__btn __large-btn mx-auto d-block'} ${classes.login_btn}`} type="submit" disabled={loading || (isEmpty(user.email) || isEmpty(user.password))}>
-                                {
-                                    loading ?
-                                        'Loading...'
-                                        :
-                                        'Log in'
-                                }
-                            </button>
-                        </form>
-                    </div>
-
-                    <p className={`${classes.content_footer_p}`}>
-                        Don't have an account? <Link to="/" className='text-decoration-underline'>Click here to Power-up!</Link>
-                    </p>
-                </div>
-            </div>
+            <HeroSection title={<>Get ready <br /> to Power-up!</>} subTitle={<>Let’s start your new experience our ground-breaking live-play games <br /> where you have the Power to control your destiny!</>} />
+            <form onSubmit={onLoginSubmit} className={formStyles.root}>
+                {
+                    !isEmpty(authUser) && !loggedIn &&
+                    <>
+                        <Alert renderMsg={() => <p>{message}</p>} danger />
+                        <br />
+                    </>
+                }
+                {
+                    !isEmpty(authUser) && loggedIn &&
+                    <>
+                        <Alert renderMsg={() => <p>Login Success</p>} success />
+                        <br />
+                    </>
+                }
+                <Input type="text" title="E-mail" required value={user.email} onChange={(e) => setUser({ ...user, email: e?.target?.value })} />
+                <Input type="password" title="Password" required value={user.password} onChange={(e) => setUser({ ...user, password: e?.target?.value })} />
+                <button className={formStyles.button} type="submit" disabled={loading || (isEmpty(user.email) || isEmpty(user.password))}>
+                    {
+                        loading ?
+                            'Loading...'
+                            :
+                            'Log in'
+                    }
+                </button>
+            </form>
+            <p className={`${styles.blogSection}`}>Don't have an account? <Link to="/power-up">Click here to Power-up!</Link></p>
             <Footer isBlack />
-            {
-                redirect()
-            }
+            {redirect()}
         </>
     )
 }

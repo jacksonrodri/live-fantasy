2ceb9c480d0014d5cbdadd7deb9628ff044eef1c
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-05-04 23:51:33 +0500

    updated

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import styles from "./content.module.scss";
+import Section from "./Section";
 
 const Content = ({contents, title, children}) => {
   return (
     <section className={styles.root}>
       <h1 className={styles.title}>{title}</h1>
-      {children}
-      {contents.map(({title, content}, index) => (
-        <section key={index} id={title.toLowerCase().replaceAll(' ', '-')}>
-          <h2 id={title}>{title}</h2>
-          {content}
-        </section>
+      <div className={styles.optionals}>{children}</div>
+      {contents.map((content, index) => (
+        <Section key={index} {...content} pageTitle={title} />
       ))}
     </section>
   );
